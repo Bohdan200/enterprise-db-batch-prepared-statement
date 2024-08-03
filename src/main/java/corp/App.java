98807910d801a10912632f583f0808fe1865667f
Bohdan <156231450+Bohdan200@users.noreply.github.com>
2024-08-03 16:02:52 +0300

    Commit

--- src/main/java/corp/App.java
+++ src/main/java/corp/App.java
@@ -1,20 +1,11 @@
 package corp;
 
 import corp.database.Database;
-import corp.operations.populate.DatabasePopulateClients;
-import corp.operations.populate.DatabasePopulateProjectWorkerRelations;
-import corp.operations.populate.DatabasePopulateProjects;
-import corp.operations.populate.DatabasePopulateWorkers;
 import corp.operations.queries.DatabaseQueryService;
 
 public class App {
     public static void main(String[] args) {
         Database database = Database.getInstance();
-
-        DatabasePopulateClients.populate();
-        DatabasePopulateProjects.populate();
-        DatabasePopulateWorkers.populate();
-        DatabasePopulateProjectWorkerRelations.populate();
 
         new DatabaseQueryService().makeQueries();
     }
